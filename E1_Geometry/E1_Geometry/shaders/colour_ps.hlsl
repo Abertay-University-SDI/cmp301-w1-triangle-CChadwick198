b8107e2bc2f24891052a66b5db6859713f658020
CChadwick198 <2309198@uad.ac.uk>
2025-09-19 14:01:55 +0100

    Completed the bonus tasks: created a black to white gradient (based off of the UV x value) on the quad.

--- E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
@@ -4,12 +4,13 @@
 struct InputType
 {
 	float4 position : SV_POSITION;
-	float4 colour : COLOR;
+	//float4 colour : COLOR;
+    float2 tex : TEXCOORD0;	// Have to name it tex as naming it texture doesn't work. Why?
 };
 
 
 float4 main(InputType input) : SV_TARGET
 {
 	//return input.colour;
-    return float4(1.f, 0.f, 0.f, 1.f); // Task 6: use the pixel shader to override the colour so the triangle is red
+    return float4((1.f * input.tex.x), (1.f * input.tex.x), (1.f * input.tex.x), 1.f); // Task 6: use the pixel shader to override the colour so the triangle is red
 }
--- E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
@@ -12,13 +12,15 @@ cbuffer MatrixBuffer : register(b0)
 struct InputType
 {
 	float4 position : POSITION;
-	float4 colour : COLOR;
+	//float4 colour : COLOR;
+    float2 tex : TEXCOORD0;
 };
 
 struct OutputType
 {
 	float4 position : SV_POSITION;
-	float4 colour : COLOR;
+	//float4 colour : COLOR;
+    float2 tex : TEXCOORD0;
 };
 
 OutputType main(InputType input)
@@ -37,7 +39,10 @@ OutputType main(InputType input)
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
-	output.colour = input.colour;
+	//output.colour = input.colour;
+	
+    output.tex = input.tex;
+    //output.colour = float4(1.f, 1.f, 1.f, 1.f);
 
 	return output;
 }
